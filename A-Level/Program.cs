02ba59f8b7d7bf55cd4bdb538ab75751208aef9f
Angelina Butenko <angelinaa2712@gmail.com>
2022-04-05 17:59:43 +0300

    Add project files.

---
+++ A-Level/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A_Level
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Anhelina";
+            string surName = "Butenko";
+            int age = 21;
+            Console.WriteLine (name + " " + surName + " " +  $"до начала новой жизни вам осталось {40 - age} лет");
+        }
+    }
+}
