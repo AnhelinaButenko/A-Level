26c4271a270fc3e8fbf88c0bf8d576702b87dd2e
Angelina Butenko <angelinaa2712@gmail.com>
2022-04-07 21:45:22 +0300

    Lesson 2 Home Work

---
+++ ArrayHW2.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayHW2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[20];
+            Random rand = new Random();
+            int[] B = new int[20];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = (rand.Next(1500));
+                Console.WriteLine($"A: {A[i]}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < B.Length; i++)
+            {
+                
+                if (A[i] <= 888)
+                {
+                    B[i] = A[i];
+                    Console.WriteLine($"B: {B[i]}");
+                }            
+            }
+
+            Console.WriteLine();
+
+            Array.Sort(B);
+            Array.Reverse(B);
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.WriteLine($"B descending: {B[i]}");
+            }
+        }
+    }
+}
---
+++ ArrayHW2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayHW2
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (rand.Next(-100, 100));
+                Console.WriteLine(array[i]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];               
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+  
+    }
+}
